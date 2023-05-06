1433563bd5f2a11b20be8b470e0c36dd1fb73b43
Alibek <132186570+sitbayevalibek@users.noreply.github.com>
2023-05-06 20:07:03 +0500

    Add files via upload

---
+++ muzey-chipta.py
@@ -0,0 +1,8 @@
+yosh = int(input("Yoshingiz nechchida?: "))
+if yosh<7 or yosh>60:
+    j = "Sizga kirish bepul"
+elif yosh<18:
+    j = "Sizga chipta narxi 5000 so'm"
+else:
+    j = "Sizga chipta narxi 10000 so'm"
+print(j)
